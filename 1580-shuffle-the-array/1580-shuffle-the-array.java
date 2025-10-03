585514834039152e1004bb71d638a4e7a2762d8d
thasneem04 <thasneem.explorer5@gmail.com>
2025-10-03 06:52:52 +0530

    Time: 0 ms (100%), Space: 44.9 MB (26.12%) - LeetHub

--- 1580-shuffle-the-array/1580-shuffle-the-array.java
+++ 1580-shuffle-the-array/1580-shuffle-the-array.java
@@ -1,38 +1,38 @@
-// class Solution {
-//     public int[] shuffle(int[] nums, int n) {
-//         int start = 0 ,mid = n,index = 0;
-//         int[] ans = new int[nums.length];
-        
-//         while(mid<nums.length)
-//         {
-//             ans[index++] = nums[start++];
-//             ans[index++] = nums[mid++];
-            
-//         }
-//         return ans;
-//     }
-// }
-
 class Solution {
     public int[] shuffle(int[] nums, int n) {
+        int start = 0 ,mid = n,index = 0;
         int[] ans = new int[nums.length];
         
-        int[] first = new int[n];       // first half
-        int[] second = new int[n];      // second half
-        
-        // split nums into two arrays
-        for(int i=0; i<n; i++) {
-            first[i] = nums[i];
-            second[i] = nums[i+n];
+        while(mid<nums.length)
+        {
+            ans[index++] = nums[start++];
+            ans[index++] = nums[mid++];
+            
         }
-        
-        // combine alternately
-        int index = 0;
-        for(int i=0; i<n; i++) {
-            ans[index++] = first[i];
-            ans[index++] = second[i];
-        }
-        
         return ans;
     }
 }
+
+// class Solution {
+//     public int[] shuffle(int[] nums, int n) {
+//         int[] ans = new int[nums.length];
+        
+//         int[] first = new int[n];       // first half
+//         int[] second = new int[n];      // second half
+        
+//         // split nums into two arrays
+//         for(int i=0; i<n; i++) {
+//             first[i] = nums[i];
+//             second[i] = nums[i+n];
+//         }
+        
+//         // combine alternately
+//         int index = 0;
+//         for(int i=0; i<n; i++) {
+//             ans[index++] = first[i];
+//             ans[index++] = second[i];
+//         }
+        
+//         return ans;
+//     }
+// }
