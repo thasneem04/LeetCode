cb3725eaf1e2f09d343bdb6f72efb817d30e71a2
thasneem04 <thasneem.explorer5@gmail.com>
2025-10-03 06:57:09 +0530

    Time: 1 ms (73.64%), Space: 41.1 MB (21.78%) - LeetHub

--- 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.java
+++ 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.java
@@ -1,30 +1,30 @@
-// class Solution {
-//     public int differenceOfSums(int n, int m) {
-        
-
-//         int num1 = 0;
-//         int num2 = 0;
-//         for(int i=0;i<=n;i++)
-//         {
-//             if(i%m!=0){
-
-//               num1 = num1 + i;  
-              
-//              }
-//              else{
-//                 num2 = num2 + i;
-//              }
-//         }
-//         return num1 - num2;
-        
-//     }
-// }
-
-public class Solution {
+class Solution {
     public int differenceOfSums(int n, int m) {
-        int totalSum = n * (n + 1) / 2;
-        int divisibleCount = n / m;
-        int divisibleSum = m * divisibleCount * (divisibleCount + 1) / 2;
-        return totalSum - 2 * divisibleSum;
+        
+
+        int num1 = 0;
+        int num2 = 0;
+        for(int i=0;i<=n;i++)
+        {
+            if(i%m!=0){
+
+              num1 = num1 + i;  
+              
+             }
+             else{
+                num2 = num2 + i;
+             }
+        }
+        return num1 - num2;
+        
     }
 }
+
+// public class Solution {
+//     public int differenceOfSums(int n, int m) {
+//         int totalSum = n * (n + 1) / 2;
+//         int divisibleCount = n / m;
+//         int divisibleSum = m * divisibleCount * (divisibleCount + 1) / 2;
+//         return totalSum - 2 * divisibleSum;
+//     }
+// }
