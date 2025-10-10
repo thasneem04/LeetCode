b104c07f02f6330710f7b8e6edddc2e27739c3c2
thasneem04 <thasneem.explorer5@gmail.com>
2025-10-10 07:37:43 +0530

    Time: 7 ms (28.01%), Space: 45.9 MB (82.85%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+       int n = nums.length;
+        Arrays.sort(nums);
+
+        if(nums.length == 1)
+        {
+            return nums[0];
+        }
+        
+
+        for(int i=0; i<n-1;i=i+2)
+        {
+            if(nums[i] != nums[i+1])
+            {
+                return nums[i];
+            }
+        }
+        return nums[nums.length-1];
+    }
+}
