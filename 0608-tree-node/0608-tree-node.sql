43a723af47dc2452eb4d2d234ca375014beee288
thasneem04 <thasneem.explorer5@gmail.com>
2025-11-16 22:14:08 +0530

    Time: 446 ms (57.16%), Space: 0B (100%) - LeetHub

---
+++ 0608-tree-node/0608-tree-node.sql
@@ -0,0 +1,10 @@
+SELECT 
+    t1.id,
+    CASE
+        WHEN t1.p_id IS NULL THEN 'Root'
+        WHEN t1.id IN (SELECT DISTINCT p_id FROM Tree WHERE p_id IS NOT NULL) THEN 'Inner'
+        ELSE 'Leaf'
+    END AS type
+FROM Tree t1
+ORDER BY t1.id;
+
